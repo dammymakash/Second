506b2f6b2a19b0fcb36e8ea9d18cc0000f6f608b
Dammy <dammymakash@gmail.com>
2025-06-18 17:01:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,108 +1,83 @@
-const lightTheme = "styles/light.css";
-const darkTheme = "styles/dark.css";
-const sunIcon = "assets/SunIcon.svg";
-const moonIcon = "assets/MoonIcon.svg";
-const themeIcon = document.getElementById("theme-icon");
-const res = document.getElementById("result");
-const toast = document.getElementById("toast");
-
-function calculate(value) {
-  const calculatedValue = eval(value || null);
-  if (isNaN(calculatedValue)) {
-    res.value = "Can't divide 0 with 0";
-    setTimeout(() => {
-      res.value = "";
-    }, 1300);
-  } else {
-    res.value = calculatedValue;
-  }
+let canvas = document.getElementById("snake"); //criar elemento que irá rodar o jogo
+let context = canvas.getContext("2d"); //....
+let box = 32;
+let snake = []; //criar cobrinha como lista, já que ela vai ser uma série de coordenadas, que quando pintadas, criam os quadradinhos
+snake[0] ={
+    x: 8 * box,
+    y: 8 * box
+}
+let direction = "right";
+let food ={
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
 }
 
-// Swaps the stylesheet to achieve dark mode.
-function changeTheme() {
-  const theme = document.getElementById("theme");
-  setTimeout(() => {
-    toast.innerHTML = "Calculator";
-  }, 1500);
-  if (theme.getAttribute("href") === lightTheme) {
-    theme.setAttribute("href", darkTheme);
-    themeIcon.setAttribute("src", sunIcon);
-    toast.innerHTML = "Dark Mode 🌙";
-  } else {
-    theme.setAttribute("href", lightTheme);
-    themeIcon.setAttribute("src", moonIcon);
-    toast.innerHTML = "Light Mode ☀️";
-  }
+function criarBG(){
+    context.fillStyle = "lightgreen";
+    context.fillRect(0, 0, 16*box, 16*box); //desenha o retângulo usando x e y e a largura e altura setadas
 }
 
-// Displays entered value on screen.
-function liveScreen(enteredValue) {
-  if (!res.value) {
-    res.value = "";
-  }
-  res.value += enteredValue;
+function criarCobrinha (){
+    for(i = 0; i < snake.length; i++){
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
 }
 
-//adding event handler on the document to handle keyboard inputs
-document.addEventListener("keydown", keyboardInputHandler);
-
-//function to handle keyboard inputs
-function keyboardInputHandler(e) {
-  // to fix the default behavior of browser,
-  // enter and backspace were causing undesired behavior when some key was already in focus.
-  e.preventDefault();
-  //grabbing the liveScreen
-
-  //numbers
-  if (e.key === "0") {
-    res.value += "0";
-  } else if (e.key === "1") {
-    res.value += "1";
-  } else if (e.key === "2") {
-    res.value += "2";
-  } else if (e.key === "3") {
-    res.value += "3";
-  } else if (e.key === "4") {
-    res.value += "4";
-  } else if (e.key === "5") {
-    res.value += "5";
-  } else if (e.key === "6") {
-    res.value += "6";
-  } else if (e.key === "7") {
-    res.value += "7";
-  } else if (e.key === "7") {
-    res.value += "7";
-  } else if (e.key === "8") {
-    res.value += "8";
-  } else if (e.key === "9") {
-    res.value += "9";
-  }
-
-  //operators
-  if (e.key === "+") {
-    res.value += "+";
-  } else if (e.key === "-") {
-    res.value += "-";
-  } else if (e.key === "*") {
-    res.value += "*";
-  } else if (e.key === "/") {
-    res.value += "/";
-  }
-
-  //decimal key
-  if (e.key === ".") {
-    res.value += ".";
-  }
-
-  //press enter to see result
-  if (e.key === "Enter") {
-    calculate(result.value);
-  }
-
-  //backspace for removing the last input
-  if (e.key === "Backspace") {
-    const resultInput = res.value;
-    //remove the last element in the string
-    res.value = resultInput.substring(0, res.value.length - 1);
-  }
+function drawFood (){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
 }
+
+//quando um evento acontece, detecta e chama uma função
+document.addEventListener('keydown', update);
+
+function update(event){
+    if(event.keyCode == 37 && direction != 'right') direction = 'left';
+    if(event.keyCode == 38 && direction != 'down') direction = 'up';
+    if(event.keyCode == 39 && direction != 'left') direction = 'right';
+    if(event.keyCode == 40 && direction != 'up') direction = 'down';
+}
+
+function iniciarJogo(){    
+
+    if(snake[0].x > 15*box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
+    if(snake[0].y > 15*box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
+    
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over :(');
+        }
+    }
+
+    criarBG();
+    criarCobrinha();
+    drawFood();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if (direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop(); //pop tira o último elemento da lista
+    }else{
+        food.x = Math.floor(Math.random() * 15 +1) * box;
+        food.y = Math.floor(Math.random() * 15 +1) * box;
+    }
+    
+    let newHead ={
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead); //método unshift adiciona como primeiro quadradinho da cobrinha
+}
+
+let jogo = setInterval(iniciarJogo, 100);
